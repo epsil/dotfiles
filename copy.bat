3c225d5fde76957227e07db8fb6f9c85ce05b5c6
Vegard Øye <vegard_oye@hotmail.com>
2018-02-09 13:45:38 +0100

    Copy script

---
+++ copy.bat
@@ -0,0 +1,10 @@
+@echo off
+cp bash_aliases ..\.bash_aliases
+cp bash_env ..\.bash_env
+cp bashrc ..\.bashrc
+cp gitconfig ..\.gitconfig
+cp rcrc ..\.rcrc
+cp vimrc ..\.vimrc
+cp zshrc ..\.zshrc
+cp emacs ..\.emacs
+cp -r emacs.d ..\.emacs.d
